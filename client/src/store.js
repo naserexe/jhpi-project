2aa166c26124a2ed0d13f21021ddb559e7bd8152
Naser <nasersoftcorporation2013@gmail.com>
2019-06-19 21:24:49 +0600

    Fix mobile view, caused by redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import { compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 
@@ -10,10 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
